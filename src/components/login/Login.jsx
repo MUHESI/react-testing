f18a8c791f633984f7c3995415b7e682641ad216
MUHESI <mvmmuhesi@gmail.com>
2023-07-27 14:53:51 +0200

    integrate logic  login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,12 +1,31 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 
 function Login() {
     const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [userName, setUserName] = useState('');
     const [pswd, setPswd] = useState('');
+    const [user, setUser] = useState({});
+    const URL = 'https://jsonplaceholder.typicode.com/users/1'
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        try {
+            const { data } = axios.get(URL)
+            setUser(data)
+
+        } catch (error) {
+            setError(true);
+
+        }
+
+
+    }
 
     return (
         <div className='container'>
+            <div>{user?.name}  </div>
             <form>
                 <input type="text" placeholder="username" value={userName}
                     onChange={(e) => setUserName(e.target.value)}
@@ -16,7 +35,8 @@ function Login() {
                 />
                 <button
                     disabled={userName === '' || pswd === ''}
-                >login</button>
+                    onClick={(e) => handleSubmit(e)}
+                >{loading ? 'loading' : 'login'}</button>
                 <span style={{ visibility: error ? 'visible' : 'hidden' }} data-testid='error'> Something went wrong</span>
 
             </form>
